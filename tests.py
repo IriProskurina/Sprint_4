3820908bf019fcbac0c073ca24dc38e0a4d5bd86
Irina Proskurina <proskurin4343@gmail.com>
2025-03-07 20:14:15 +0700

    modified tests.py

--- tests.py
+++ tests.py
@@ -17,5 +17,19 @@ class TestBooksCollector:
         collector.add_new_book("Гордость и предубеждение и зомби")
         collector.add_new_book("Что делать, если ваш кот хочет вас убить")
 
+        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
+
+class TestBooksCollectorNew:
+    @pytest.fixture
+    def collector(self):
+        return BooksCollector()
+
+    @pytest.mark.parametrize(
+        "books_name, expected",
+        [("Война миров", True), ("Сияние", True), ("", False), ("A" * 41, False)],
+    )
+    def test_add_new_book(self, collector, book_name, expected):
+        collector.add_new_book(book_name)
+        assert(book_name in collector.get_books_genre()) == expected
